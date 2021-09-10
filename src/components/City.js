8231ed88e664645bd9aae7f892279118ac3fdb60
Jonas Mehlich <jonas.mehlich@veda.net>
2021-09-10 19:14:11 +0200

    mouse hover

--- src/components/City.js
+++ src/components/City.js
@@ -3,7 +3,7 @@ import React from 'react'
 const City = ({data, onClick}) => {
 
     return (
-        <div key={data.link} onClick={(e) => onClick(data)}>{data.name}</div>
+        <div className="city" key={data.link} onClick={(e) => onClick(data)}>{data.name}</div>
     )
 }
 
