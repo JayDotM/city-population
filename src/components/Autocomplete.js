9fc112ba6fafddef32d6c24904553952fa897bf1
Jonas Mehlich <jonas.mehlich@veda.net>
2021-09-17 15:22:26 +0200

    - use await - use one city object for state

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -3,47 +3,42 @@ import { useState } from 'react'
 import City from './City';
 
 
-const Autocomplete = ({onCityClick}) => {
+const Autocomplete = ({ onCityClick }) => {
     const [cities, setCities] = useState([]);
-    const [population, setPopulation] = useState();
     const [searchText, setSearchText] = useState('');
-    const [cityName, setCityName] = useState('');
+    const [{population, cityName}, setCity] = useState({population: '', cityName: ''});
 
-    const onChange = (e) => {
+    async function onChange(e) {
         setSearchText(e.target.value)
-        // get cities
-        fetch('https://api.teleport.org/api/cities/?search=' + e.target.value)
-            .then(result => result.json())
-            .then((data) => {
-                const apiCities = data._embedded['city:search-results'];
-                setCities(apiCities.map((city) => {
-                    return {
-                        'name': city.matching_full_name,
-                        'link': city._links['city:item'].href
-                    }
-                }))
-            })
-            .catch(console.log)
+        const searchEndpoint = 'https://api.teleport.org/api/cities/?search=';
+        const searchResult = await fetch(searchEndpoint + e.target.value).then(result => result.json());
+        const apiCities = searchResult._embedded['city:search-results'];
+        setCities(apiCities.map((city) => {
+            return {
+                'name': city.matching_full_name,
+                'link': city._links['city:item'].href
+            }
+        }))
     };
 
-    const onClickCity = (data) => {
-        fetch(data.link)
-            .then(result => result.json())
-            .then((data) => {
-                setPopulation(data.population)
-                setCityName(data.full_name)
-                setCities([])
-                setSearchText('')
-                onCityClick(data.population)
-            })
-            .catch(console.log)
+    async function onClickCity(data) {
+        const cityData = await fetch(data.link).then(result => result.json());
+        setCity({
+            population: cityData.population, 
+            cityName: cityData.full_name
+        });
+
+        setCities([])
+        setSearchText('')
+
+        onCityClick(cityData.population)
     };
 
     return (
         <div>
             <div style={{ display: "flex" }}>
                 <input type="text" placeholder="Search for a city"
-                    onChange={onChange} value={searchText}/>
+                    onChange={onChange} value={searchText} />
                 <label>Population: </label>
                 <div>{population}</div>
                 {cityName && <div>({cityName})</div>}
